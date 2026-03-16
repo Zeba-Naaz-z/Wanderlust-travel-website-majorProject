bb1d184516ff6767f61fcb6f92a2f321e7032f3f
Zeba-Naaz-z <naazzeba711@gmail.com>
2026-03-16 06:18:21 +0530

    fix owner null error

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
+const { Types } = mongoose;
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
   .then( () => {
@@ -20,7 +23,7 @@ const initDB = async () => {
     await Listing.deleteMany( {} );
     initData.data = initData.data.map((obj) => ({
     ...obj, 
-    owner: "69aa8421305c93fe518ba6f4",
+    owner: new Types.ObjectId("69aa8421305c93fe518ba6f4"),
    }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
